75f72a310d78384e4f9e1f6b038e2064ef93c0eb
Johann Schroeder <jschroeder89@email.com>
2018-01-09 14:50:50 +0100

    binary2gray has pop as argument

--- Codierungsfunktionen/gray2binary.m
+++ Codierungsfunktionen/gray2binary.m
@@ -1,16 +1,22 @@
-function [binary_coded] = gray2binary(Cg)
+function [binary_pop] = gray2binary(Cg)
+popsize = length(pop(1,:));
+nbits = length(pop(1,1));
+    for j = 1:2
+        for i = 1:popsize
+            Cg = pop(j, i);
+            if class(Cg) == 'cell'
+                Cg = char(Cg);
+            end
 
-if class(Cg) == 'cell'
-    Cg = char(Cg);
-end
+            nbits = length(Cg);
+            binary_coded(1) = Cg(1);
 
-nbits = length(Cg);
-binary_coded(1) = Cg(1);
-
-for i = 2:nbits
-    Cgray = str2num(Cg(i));
-    Cbin = str2num(binary_coded(i-1));   
-    binary_coded(i) = num2str(xor(Cgray, Cbin)); 
-end    
-
+            for i = 2:nbits
+                Cgray = str2num(Cg(i));
+                Cbin = str2num(binary_coded(i-1));   
+                binary_coded(i) = num2str(xor(Cgray, Cbin)); 
+            end
+            binary_pop(j,i) = binary_coded;         
+        end
+    end
 end
