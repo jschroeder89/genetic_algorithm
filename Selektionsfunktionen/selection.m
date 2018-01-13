996eb2cbf828fccf663307e89f00e67c7f365fd0
Johann Schroeder <jschroeder89@email.com>
2018-01-13 15:44:29 +0100

    updated selection container

--- Selektionsfunktionen/selection.m
+++ Selektionsfunktionen/selection.m
@@ -1,6 +1,8 @@
-function sel = selection(sel_function, pop)
+function sel = selection(sel_function, n, pop)
     switch sel_function
     case 'rank_base'
         sel = rank_base(pop);
+    case 'elite'
+        sel = elite_sel(n, pop)
     end
 end
